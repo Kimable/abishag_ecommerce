e5cabd00a2f167acde9d3b58d5db61fd3bc37baa
Kelvin Kimani <kkimani@yahoo.com>
2024-12-24 16:02:06 +0300

    Added major CRUD functionalities for managing products

---
+++ admin/orders.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/../util/DB_connect.php';
+require './functions/ManageOrders.php';
+
+require  './includes/admin_head.php';
+?>
+
+<h1>Orders</h1>
+
+<?php require './includes/admin_footer.php' ?>
---
+++ admin/revenue.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/../util/DB_connect.php';
+
+require  './includes/admin_head.php';
+?>
+
+<h1>Revenue</h1>
+
+<?php require './includes/admin_footer.php' ?>
